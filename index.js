76d8b090a9cccb92f2ce2a93145ca04229e8b111
Craig Spaeth <craigspaeth@Craigs-iMac.local>
2015-09-14 18:19:10 -0400

    refactorz

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import express from 'express'
 import artworks from 'artworks'
 import d from 'debug'
 import errorHandler from 'middleware/error'
+let debug = d('app')
 
 let { PORT } = process.env
-let debug = d('app')
+
 let app = express()
 
 app.use(artworks)
 app.use(errorHandler)
-
 app.listen(PORT, () => debug(`Listening on ${PORT}`))
