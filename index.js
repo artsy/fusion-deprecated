73257a716944fae5430112a0deb9c18ed0bdff8e
Craig Spaeth <craigspaeth@Craigs-iMac.local>
2015-09-14 17:03:45 -0400

    err handler

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import 'newrelic'
 import express from 'express'
 import artworks from 'artworks'
 import d from 'debug'
+import errorHandler from 'middleware/error'
 
 let { PORT } = process.env
 let debug = d('app')
 let app = express()
 
 app.use(artworks)
+app.use(errorHandler)
 
 app.listen(PORT, () => debug(`Listening on ${PORT}`))
