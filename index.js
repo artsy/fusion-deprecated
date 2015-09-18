477cdac602cc50282370b6bfe2a222c6f14edf1e
Craig Spaeth <craigspaeth@Craigs-iMac.local>
2015-09-18 16:39:34 -0400

    proxy gravity otherwies

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ import d from 'debug'
 import errorHandler from 'middleware/error'
 import artsyXapp from 'artsy-xapp'
 import morgan from 'morgan'
+import proxyGravity from 'middleware/proxy-gravity'
 let debug = d('app')
-
-let { PORT } = process.env
+let { PORT, ARTSY_URL } = process.env
 
 let app = express()
 
 app.use(artworks)
 app.use(errorHandler)
 app.use(morgan('dev'))
+app.use(proxyGravity)
 
 artsyXapp.on('error', debug).init(() => {
   app.listen(PORT, () => debug(`Listening on ${PORT}`))
